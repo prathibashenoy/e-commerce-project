8a92dd19bce1ae9eae3ba55fb7fc594ce40e3a01
prathiba <prathibashenoy10@gmail.com>
2026-01-11 11:20:57 +0400

    Fix Cloudinary self-import issue

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,21 +1,14 @@
-import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
+//config/cloudinary.js
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: (req) => ({
-    folder: req.baseUrl.includes("product")
-      ? "ecommerce/products"
-      : "ecommerce/categories",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
-    resource_type: "image",
-  }),
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const upload = multer({
-  storage,
-  limits: {
-    fileSize: 2 * 1024 * 1024, // 2MB
-  },
-});
+console.log("Cloudinary connected:", process.env.CLOUDINARY_CLOUD_NAME);
+
+
+export default cloudinary;
